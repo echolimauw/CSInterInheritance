0121f1c3f1c3283b88e14d71930f0bfa19ab0d29
Evan Leahy <49353272+echolimauw@users.noreply.github.com>
2021-05-10 14:19:59 -0700

    Work with Access Modifiers
    started new repo for inheritance practice and began setting up classes with access modifiers

---
+++ CSIntermediateInheritance/CSIntermediateInheritance/Customer.cs
@@ -0,0 +1,25 @@
+﻿namespace CSIntermediateInheritance
+{
+    partial class Program
+    {
+        public class Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public void Promote()
+            {
+                var rating = CalculateRating();
+                if (rating == 0)
+                    System.Console.WriteLine("Promoted to Level 1");
+                else
+                    System.Console.WriteLine("Promoted to Level 2");
+            }
+
+            public int CalculateRating()
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ CSIntermediateInheritance/CSIntermediateInheritance/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSIntermediateInheritance
+{
+    partial class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var customer = new Customer;
+        }
+    }
+}
